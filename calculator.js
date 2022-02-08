7ba62f1acd100ba333d9ce3eab64bf5236e1d095
JHokum <jdelgado92@hotmail.com>
2022-02-08 13:42:45 -0800

    Operator functions Functions for adding, subtracting, multiplying, dividing

---
+++ calculator.js
@@ -0,0 +1,13 @@
+const add = (num1, num2) => num1+num2
+const subtract = (num1, num2) => num1-num2
+const multiply = (num1, num2) => num1*num2
+const divide = (num1, num2) => num1/num2
+
+function operate(operator, num1, num2){
+    //switch
+        //case add
+        //case subtract
+        //case multiply
+        //case divide
+}
+
