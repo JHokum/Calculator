37cb2567b461a3e1a8f02a6cc09c585ca691169f
JHokum <jdelgado92@hotmail.com>
2022-02-09 16:57:16 -0800

    Event Listeners, Keys and Operators Number keys currently display their textContent to display and set global number variables for operations. Operator keys set global operation variable and display textContent.

--- calculator.js
+++ calculator.js
@@ -3,6 +3,11 @@ const subtract = (num1, num2) => num1-num2
 const multiply = (num1, num2) => num1*num2
 const divide = (num1, num2) => num1/num2
 
+
+//globals
+let num1 = 0;
+let num2 = 0;
+let operator="";
 function operate(operator, num1, num2){
     //switch
         //case add
@@ -12,7 +17,78 @@ function operate(operator, num1, num2){
 }
 
 const buttons = Array.from(document.querySelectorAll("button"));
-buttons.forEach(button=>button.addEventListener("click",(e)=>display.textContent+=e.target.textContent))
+// buttons.forEach(button=>button.addEventListener("click",(e)=>display.textContent+=e.target.textContent))
+const equals = document.querySelector(".equals");
+//clearfunctionality
+const clearDisplay = ()=>{
+    display.textContent="";
+}
+//equals functionality
+const equalsFunc = (operator, num1, num2)=>{
+    
+    switch(operator){
+        case("divide"):
+            return num1/num2;
+        case("multiply"):
+            return num1*num2;
+        case("add"):
+            return num1+num2;
+        case("subtract"):
+            return num1-num2;
+    }
+}
+
+//Operations
+const performOperation = function(str){
+    changeDisplay("Operation Complete!");
+}
+
+// Change Display
+const changeDisplay = function(str){
+    display.textContent+=str;
+}
+
+//number key function
+const numFunc = function(str){
+    number = +str;
+    if (num1==0){
+        num1=number;
+        changeDisplay(str);
+    }
+    else{
+        num2=number;
+        changeDisplay(str);
+    }
+}
+//operator function
+const opFunc = function(event){
+    changeDisplay(targetContent(event));
+    operator=targetData(event);
+    
+}
+
+//targeting helper functions
+const targetContent = function(event){
+    return event.target.textContent;
+}
+const targetData = function(event){
+    return event.target.dataset.operation;
+}
+//buttons
+const addListeners = function(buttons){
+    buttons.forEach(function(button){
+        if (button.classList.contains("num")){
+            button.addEventListener("click",e=>numFunc(targetContent(e)));
+        } 
+        else if(button.classList.contains("operator")){
+            button.addEventListener("click",e=>opFunc(e))
+        }
+        else if(button.classList.contains("equals")){
+            button.addEventListener("click",()=>changeDisplay(equalsFunc))
+        }   
+        })
+}
+addListeners(buttons);
 
 const display = document.querySelector(".display");
 
