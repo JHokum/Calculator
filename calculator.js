46f005197f87a6249ba5b548fadac17054870116
JHokum <jdelgado92@hotmail.com>
2022-02-10 17:38:59 -0800

    Snarky divide by 0 Dividing by 0 destroys the universe, press C to restore.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,13 @@
 const add = (num1, num2) => rounding(num1+num2)
 const subtract = (num1, num2) => rounding(num1-num2)
 const multiply = (num1, num2) => rounding(num1*num2)
-const divide = (num1, num2) => rounding(num1/num2)
+const divide = (num1, num2) => {
+    console.log(num2);
+    if(num2==0){
+        return "Press C to Restore Universe";
+    }
+    return rounding(num1/num2)
+}
 
 
 //globals
